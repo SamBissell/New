4e07ffa8f749c18c9a6823dad4c736fff3b65f60
Sam Bissell <sam.bissell@gmail.com>
2021-05-14 14:44:47 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,38 +4,44 @@ const submitBtn = document.getElementById("submit");
 const inputField = document.getElementById("todoInput");
 const inputPriority = document.getElementById("priority");
 const todos = document.getElementById("todo-list");
-const deleteBtn = document.querySelectorAll(".delete");
+const doneButton = document.querySelectorAll(".done");
+const clearBtn = document.getElementById('clear-all');
+
+
 // const clearBtn = document.getElementById("clear");
 
 //==== Model
 
-let list = [];
+
 
 class Todo {
   constructor(text) {
-    this.id = list.length + 1;
+
     this.todoText = text;
     // this.priority = priority;
   }
 
-  addToList() {
-    list.push(this);
-  }
+
 
   renderTodo() {
     const newTodo = document.createElement("div");
     const newText = document.createTextNode(`${this.todoText}`);
-    const deleteBtn = document.createElement("BUTTON");
-    newTodo.id = `${this.id}`;
+    const doneButton = document.createElement("BUTTON");
+    doneButton.addEventListener('click', doneTodo);
+    const deleteButton = document.createElement("BUTTON");
+    deleteButton.addEventListener('click', deleteTodo);
     newTodo.classList.add("todo");
-    deleteBtn.classList.add("delete");
-    deleteBtn.innerHTML = "Done";
+    doneButton.classList.add("done");
+    doneButton.innerHTML = "Done";
+    deleteButton.classList.add("delete");
+    deleteButton.innerHTML = "Delete";
 
     newTodo.appendChild(newText);
-    newTodo.appendChild(deleteBtn);
+    newTodo.appendChild(doneButton);
+    newTodo.appendChild(deleteButton);
     todos.appendChild(newTodo);
 
-    deleteBtn.addEventListener("click", deleteTodo);
+    doneButton.addEventListener("click", doneTodo);
   }
 }
 
@@ -43,23 +49,28 @@ class Todo {
 const newTodo = function () {
   let text = inputField.value;
   const todo = new Todo(text);
-  todo.addToList();
   todo.renderTodo();
   inputField.value = "";
-  console.log(list);
+};
+
+const doneTodo = (e) => {
+  let targetedTodo = e.target.closest(".todo");
+  let targeteddone = e.target.closest(".done");
+  // console.log(`${targetedTodo.id}`);
+  // remove from array
+
+  // list.splice(targetedIndex, 1);
+  // console.log(list);
+  // change styling
+  targetedTodo.classList.add("strike");
+  // targeteddone.classList.add("hide");
 };
 
 const deleteTodo = (e) => {
   let targetedTodo = e.target.closest(".todo");
-  let targetedDelete = e.target.closest(".delete");
-  console.log(`${targetedTodo.id}`);
-  // remove from array
-  let targetedIndex = targetedTodo - 1;
-  list.splice(targetedIndex, 1);
-  // console.log(list);
-  // change styling
-  targetedTodo.classList.add("strike");
-  targetedDelete.classList.add("hide");
+
+  targetedTodo.remove();
+  // targeteddone.classList.add("hide");
 };
 
 submitBtn.addEventListener("click", (e) => {
@@ -72,7 +83,10 @@ submitBtn.addEventListener("click", (e) => {
   }
 });
 
-// clearBtn.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   // a function that removes all todos
-// });
+clearBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  // a function that removes all todos
+  while (todos.firstChild) {
+    todos.removeChild(todos.lastChild);
+  }
+});
